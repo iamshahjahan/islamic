4ff4149fd0ea50447654c2d145e07d59ba3d0bc4
shiza1aas <ahmsjahan@gmail.com>
2016-04-11 11:14:39 +0000

    added to pythonanywhere

--- islamic/settings.py
+++ islamic/settings.py
@@ -126,5 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = "/home/quoteofmuslims/islamic/mystatic/"
 STATIC_URL = '/static/'
+
--- whoismuhammad/urls.py
+++ whoismuhammad/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url,patterns
 from whoismuhammad import views
 
-urlpatterns = patterns('',url(r'^$', views.index, name='index'))
+urlpatterns = patterns('',
+			url(r'^$', views.index, name='index')
+			)
 
